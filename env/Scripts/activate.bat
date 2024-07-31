6a6d75ebb7f4d8beff8038dfc3065bd1f8faf357
kurbashh <arturkurbanov111@mail.ru>
2024-07-31 18:27:38 +0500

    Initial commit

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Admin\Desktop\gatob\env
+set VIRTUAL_ENV=C:\Users\artur\OneDrive\Рабочий стол\gatob13\env
 
 if not defined PROMPT set PROMPT=$P$G
 
